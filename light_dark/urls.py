a3548b8fd3b99a2d4b3526ccafd6bb6f2d3c2ace
bunvin <bunvin@gmail.com>
2022-02-24 22:58:55 +0200

    css added, greet

--- light_dark/urls.py
+++ light_dark/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views #import the function from views
 
 urlpatterns = [
-    path("",views.index, name="index") #index is the function name in views.py
+    path("",views.index, name="index"), #index is the function name in views.py
+    path("bunvin",views.bunvin, name="bunvin"),
+    path("<str:name>", views.greet, name="greet") #name is what's after /
 ]
--- light_dark/views.py
+++ light_dark/views.py
@@ -3,5 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request): #represent the http request
-    return HttpResponse("Hello, world")
-#but when do you response? urls.py -> manually added
+    return render(request, "light_dark/index.html")
+    #return render, (request, "light_dark/index.html")
+#but when do you response? urls.py -> manually added when route will
+
+def bunvin(request):
+    return HttpResponse("Hello, Bun!")
+
+#def greet(request, name):
+ #   return HttpResponse(f"Hello, {name.capitalize()}!")
+
+def greet(request, name):
+    return render(request, "light_dark/greet.html", {"name": name.capitalize()})
